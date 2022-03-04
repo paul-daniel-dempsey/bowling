2f18256691f6e9f26040f5383f99f5a064dc60f3
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-03-04 16:01:41 +0000

    TestA : Sum All - No strikes or spares < 10

--- src/bowling.js
+++ src/bowling.js
@@ -1,6 +1,31 @@
 
-const bowl = (frames) => {
-    return 90;
+const bowl = (game) => {
+    return bowlTestA(game);
+    // return bowlTestB(game);
+    // return bowlTestC(game);
+    // return bowlTestD(game);
+    // return bowlTestE(game);
+    // return bowlRefactor(game);    
 }
+
+const bowlTestA = (game) => {
+
+    // V1
+    // let total = 0;
+    // game.forEach(frame => {total = total + frame.reduce((lhs, thrw) => lhs + thrw,0);});
+    // return total;
+
+    // V2
+    return game.reduce((lhs, frame) => lhs + frame.reduce((lhs, thrw) => lhs + thrw,0),0)
+};
+
+const bowlTestB = (game) => {};
+const bowlTestC = (game) => {};
+const bowlTestD = (game) => {};
+const bowlTestE = (game) => {};
+const bowlRefactor = (game) => {};
+
+
+
 
 module.exports = {bowl};
