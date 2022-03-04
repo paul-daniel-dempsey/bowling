2f3d371095a97d23dbaa1f3e5189bf0c91f7bd86
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-03-04 17:17:48 +0000

    TestC : Strike (first throw = 10) in first 9 frames

--- src/bowling.js
+++ src/bowling.js
@@ -1,8 +1,8 @@
 
 const bowl = (game) => {
     //return bowlTestA(game);
-    return bowlTestB(game);
-    // return bowlTestC(game);
+    //return bowlTestB(game);
+    return bowlTestC(game);
     // return bowlTestD(game);
     // return bowlTestE(game);
     // return bowlRefactor(game);    
@@ -48,7 +48,25 @@ const bowlTestB = (game) => {
     return totalGame;
 };
 
-const bowlTestC = (game) => {};
+const bowlTestC = (game) => {
+    let totalGame = 0;
+    let totalFrame = 0;
+    let spare = 1;
+    let strike = 0;
+    game.forEach(frame => {
+        frame.forEach((thrw,index) => {
+            totalFrame += (thrw * spare);
+            spare = ((totalFrame === 10 && index===1 && strike !== 10) ? 2 : 1);
+            strike = ((totalFrame === 10 && index===0) ? 10 : strike);
+            console.log(`totalFrame ${totalFrame} thrw ${thrw} index ${index} multiplySpare ${spare} strike ${strike}`)
+        });
+        totalGame += totalFrame + strike;
+        console.log(`totalGame ${totalGame}`);
+        strike = 0
+        totalFrame = 0;
+    });
+    return totalGame;
+};
 const bowlTestD = (game) => {};
 const bowlTestE = (game) => {};
 const bowlRefactor = (game) => {};
