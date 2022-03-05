0141a1534c3d65c60348b5755d38b3a5c244140a
Paul.Dempsey <paul.daniel.dempsey@icloud.com>
2022-03-05 21:09:50 +0000

    Refactored, cleanup undefined tests, comments, removed debug, shorter line count

--- src/bowling.js
+++ src/bowling.js
@@ -3,8 +3,8 @@ const bowlgame = (game) => {
     //return bowlgameTestB(game);
     //return bowlgameTestC(game);
     //return bowlgameTestD(game);
-    return bowlgameTestE(game);
-    //return bowlgameRefactor(game);    
+    //return bowlgameTestE(game);
+    return bowlgameRefactor(game);    
 }
 
 // (A) GAME of 20 THROWS or 10 FRAMEs
@@ -122,6 +122,30 @@ const bowlgameTestE = (throws) => {
 }
 
 
-const bowlgameTestRefactor = (throws) => {}
+const bowlgameRefactor = (throws) => {
+ 
+    let gameScore = 0;
+    for(i=0;i<throws.length; i++) {  // Arrow function wont go forward 1/2 places to get spares/strikes
+        frame = throws[i]
+        if (frame === 10) {
+            // Strike
+            gameScore += frame + ((throws[i+1]) ? throws[i+1] : 0) + ((throws[i+2]) ? throws[i+2] : 0)
+        }
+        else {
+            frame += ((throws[i+1]) ? throws[i+1] : 0);
+            if (frame === 10) {
+                // Spare
+                gameScore += frame + ((throws[i+2]) ? throws[i+2] : 0)
+            }
+            else {
+                // Pair
+                gameScore += frame
+            }
+        }
+        // Skip over 2 throws (Spare/Pair) if not Strike
+        i += (throws[i] !== 10 ? 1 : 0);
+    }
+    return gameScore;    
+}
 
 module.exports = {bowlgame};
